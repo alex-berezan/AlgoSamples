ddf162d4417bbb568741f0fb34ccab441c549251
aleksey-berezan <aleksey.berezan@gmail.com>
2012-10-17 01:53:13 +0300

    Implemented InsertSort

---
+++ Sorting/InsertSort/Facts.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Sorting.InsertSort
+{
+	public class Facts
+	{
+		[Theory]
+		[InlineData(null)]
+		[InlineData(new int[] { })]
+		public void Returns_Empty_Array_When_Empty_Or_Null_One_Is_Specified(int[] input)
+		{
+			var sorter = new InsertSorter();
+
+			int[] actual = sorter.Sort(input);
+
+			actual.Should().BeEquivalentTo(new int[] { });
+		}
+
+		[Fact]
+		public void Returns_Equivalent_Array_For_Single_Element_Input()
+		{
+			var sorter = new InsertSorter();
+
+			int[] actual = sorter.Sort(new int[] { 42 });
+
+			actual.Should().BeEquivalentTo(new int[] { 42 });
+		}
+
+		[Fact]
+		public void Returns_Array_Sorted_By_Asc()
+		{
+			var sorter = new InsertSorter();
+
+			int[] actual = sorter.Sort(new int[] { 1, 5, 3, 8, 2, 11 });
+
+			actual.Should().BeEquivalentTo(new int[] { 1, 2, 3, 5, 8, 11 });
+			actual.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void Returns_Array_Sorted_By_Asc_For_Input_With_Duplicates()
+		{
+			var sorter = new InsertSorter();
+
+			int[] actual = sorter.Sort(new int[] { 11, 1, 5, 3, 8, 2, 11 });
+
+			actual.Should().BeEquivalentTo(new int[] { 1, 2, 3, 5, 8, 11, 11 });
+			actual.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void Sort_Specified_Array_By_Asc()
+		{
+			var sorter = new InsertSorter();
+
+			var actual = new int[] { 1, 5, 3, 8, 2, 11 };
+			var expected = new int[] { 1, 2, 3, 5, 8, 11 };
+
+			sorter.SortArray(actual);
+
+			actual.Should().BeEquivalentTo(expected);
+			actual.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void Does_Not_Change_Input_Array()
+		{
+			var sorter = new InsertSorter();
+
+			var input = new int[] { 1, 5 };
+			int[] actual = sorter.Sort(input);
+
+			Assert.NotSame(input, actual);
+		}
+	}
+}
---
+++ Sorting/InsertSort/InsertSorter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting.InsertSort
+{
+	public class InsertSorter
+	{
+		public int[] Sort(int[] array)
+		{
+			if (array == null || array.Length == 0)
+				return new int[] { };
+
+			int[] result = new int[array.Length];
+			array.CopyTo(result, 0);
+
+			SortArray(result);
+			return result;
+		}
+
+		public void SortArray(int[] array)
+		{
+			if (array == null || array.Length == 0)
+				return;
+
+			for (int currentIndex = 0; currentIndex < array.Length - 1; currentIndex++)
+			{
+				var minValueIndex = GetMinValueIndex(array, startIndex: currentIndex);
+				SwapArrayValues(array, currentIndex, minValueIndex);
+			}
+		}
+
+		private static int GetMinValueIndex(int[] array, int startIndex)
+		{
+			int minIndex = startIndex;
+			for (int right = startIndex; right < array.Length; right++)
+			{
+				int current = array[right];
+				if (current < array[minIndex])
+				{
+					minIndex = right;
+				}
+			}
+			return minIndex;
+		}
+
+		private static void SwapArrayValues(int[] array, int index1, int index2)
+		{
+			int temp = array[index1];
+			array[index1] = array[index2];
+			array[index2] = temp;
+		}
+	}
+}
