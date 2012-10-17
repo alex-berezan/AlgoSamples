af498926c8f68a7dff3dc1351acfdc43cb934300
aleksey-berezan <aleksey.berezan@gmail.com>
2012-10-18 00:58:46 +0300

    Added MergeSort

---
+++ Sorting/MergeSort/Facts.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Sorting.MergeSort
+{
+	public class Facts
+	{
+		[Theory]
+		[InlineData(null)]
+		[InlineData(new int[] { })]
+		public void Returns_Empty_Array_When_Empty_Or_Null_One_Is_Specified(int[] input)
+		{
+			var sorter = new MergeSorter();
+
+			int[] actual = sorter.Sort(input);
+
+			actual.Should().BeEquivalentTo(new int[] { });
+		}
+
+		[Fact]
+		public void Returns_Equivalent_Array_For_Single_Element_Input()
+		{
+			var sorter = new MergeSorter();
+
+			int[] actual = sorter.Sort(new int[] { 42 });
+
+			actual.Should().BeEquivalentTo(new int[] { 42 });
+		}
+
+		[Fact]
+		public void Returns_Array_Sorted_By_Asc()
+		{
+			var sorter = new MergeSorter();
+
+			int[] actual = sorter.Sort(new int[] { 1, 5, 3, 8, 2, 11 });
+
+			actual.Should().BeEquivalentTo(new int[] { 1, 2, 3, 5, 8, 11 });
+			actual.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void Returns_Array_Sorted_By_Asc_For_Array_Where_All_Elements_Are_The_Same()
+		{
+			var sorter = new MergeSorter();
+
+			int[] actual = sorter.Sort(new int[] { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 });
+
+			actual.Should().BeEquivalentTo(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+			actual.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void Returns_Array_Sorted_By_Asc_For_Input_With_Duplicates()
+		{
+			var sorter = new MergeSorter();
+
+			int[] actual = sorter.Sort(new int[] { 11, 1, 5, 8, 8, 8, 8, 8, 3, 8, 2, 8, 8, 11, 8, });
+
+			actual.Should().BeEquivalentTo(new int[] { 1, 2, 3, 5, 8, 8, 8, 8, 8, 8, 8, 8, 8, 11, 11 });
+			actual.Should().BeInAscendingOrder();
+		}
+
+		[Fact]
+		public void Returns_Copy_Of_Input_Array()
+		{
+			var sorter = new MergeSorter();
+
+			var input = new int[] { 1, 5 };
+			int[] actual = sorter.Sort(input);
+
+			actual.Should().NotBeNull();
+			Assert.NotSame(input, actual);
+		}
+	}
+}
---
+++ Sorting/MergeSort/MergeSorter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sorting.Helpers;
+
+namespace Sorting.MergeSort
+{
+	public class MergeSorter
+	{
+		public int[] Sort(int[] array)
+		{
+			if (array == null || array.Length == 0)
+				return new int[] { };
+
+			if (array.Length == 1)
+				return new List<int>(array).ToArray();
+
+			if (array.Length == 2)
+			{
+				return array[1] < array[0]
+					? new[] { array[1], array[0] }
+					: new[] { array[0], array[1] };
+			}
+
+			int half = array.Length / 2;
+
+			int[] left = array.Take(half).ToArray();
+			int[] right = array.Skip(half).Take(array.Length - half).ToArray();
+
+			int[] leftSorted = Sort(left);
+			int[] rightSorted = Sort(right);
+
+			int[] result = MergeSorted(leftSorted, rightSorted);
+
+			return result;
+		}
+
+		private int[] MergeSorted(int[] left, int[] right)
+		{
+			int[] result = new int[left.Length + right.Length];
+
+			int leftCurrent = 0;
+			int rightCurrent = 0;
+
+			for (int i = 0; i < result.Length; i++)
+			{
+				bool outOfLeft = !(leftCurrent < left.Length);
+				bool outOfRight = !(rightCurrent < right.Length);
+
+				if (!outOfLeft && (outOfRight || left[leftCurrent] < right[rightCurrent]))
+				{
+					result[i] = left[leftCurrent];
+					leftCurrent++;
+				}
+				else
+				{
+					result[i] = right[rightCurrent];
+					rightCurrent++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
