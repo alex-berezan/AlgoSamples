4c870337ef7acb65d8ee26c1e9c68dd5cf611edb
aleksey-berezan <aleksey.berezan@gmail.com>
2012-10-19 01:32:04 +0300

    Added benchmark console app

---
+++ AlgoSamples/Benchmark/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Sorting.MergeSort;
+
+namespace Benchmark
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchPerformance();
+			// BenchCorrectness();
+
+			Console.WriteLine("Hit any key");
+			Console.ReadKey(intercept: true);
+		}
+
+		private static void BenchCorrectness()
+		{
+			int iterationsCount = 20;
+			int arraySize = 20000;
+
+			var mergeSorter = new MergeSorter();
+			var elapsed1 = DoTest(iterationsCount, () =>
+			{
+				var array = GenerateTestArray(arraySize);
+				array = mergeSorter.Sort(array);
+				array.Should().BeInAscendingOrder();
+
+			});
+			Console.WriteLine("Elapsed1: {0}", elapsed1);
+
+			var mergeSorter2 = new Sorting.MergeSort2.MemoryLoyalMergeSort();
+			var elapsed2 = DoTest(iterationsCount, () =>
+			{
+				var array = GenerateTestArray(arraySize);
+				mergeSorter2.Sort(array);
+				array.Should().BeInAscendingOrder();
+			});
+			Console.WriteLine("Elapsed2: {0}", elapsed2);
+		}
+
+		private static void BenchPerformance()
+		{
+			int iterationsCount = 40000;
+			int arraySize = 25000;
+
+			var array = GenerateTestArray(arraySize);
+
+			//var mergeSorter = new MergeSorter();
+			//var elapsed1 = DoTest(iterationsCount, () => mergeSorter.Sort(array));
+			//Console.WriteLine("Elapsed1: {0}", elapsed1);
+
+			var mergeSorter2 = new Sorting.MergeSort2.MemoryLoyalMergeSort();
+			var elapsed2 = DoTest(iterationsCount, () => mergeSorter2.Sort(array));
+			Console.WriteLine("Elapsed2: {0}", elapsed2);
+		}
+
+		private static int[] GenerateTestArray(int arraySize)
+		{
+			Random r = new Random(42);
+			List<int> input = new List<int>();
+			for (int i = 0; i < arraySize; i++)
+			{
+				input.Add(i * r.Next(0, 42));
+			}
+
+			int[] array = input.ToArray();
+			return array;
+		}
+
+		private static TimeSpan DoTest(int iterationsCount, Action iteration)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			for (int i = 0; i < iterationsCount; i++)
+			{
+				iteration();
+			}
+			sw.Stop();
+			return sw.Elapsed;
+		}
+	}
+}
