bab98a1c491fe13680abec555343ea78d5d18a4a
aleksey-berezan <aleksey.berezan@gmail.com>
2013-03-17 15:01:27 +0200

    small modification

--- AlgoSamples/Trees/FindKthSmallestItem/FindKthSmallestItemImpl.cs
+++ AlgoSamples/Trees/FindKthSmallestItem/FindKthSmallestItemImpl.cs
@@ -1,12 +1,63 @@
-﻿using Trees.ResolveParentsDuplicates;
+﻿using System;
+using System.Diagnostics;
+using Trees.ResolveParentsDuplicates;
 
 namespace Trees.FindKthSmallestItem
 {
 	public class FindKthSmallestItemImpl
 	{
-		public int  Find(TreeNode<int> root, int k)
+		[DebuggerDisplay("{Result}, {Max}, {Count}")]
+		private struct SearchResult
 		{
-			throw new System.NotImplementedException();
+			public readonly int? Result;
+			public readonly int? Max;
+			public readonly int Count;
+
+			[DebuggerStepThrough]
+			public SearchResult(int? result, int? max, int count)
+				: this()
+			{
+				Result = result;
+				Max = max;
+				Count = count;
+			}
+			public SearchResult Union(SearchResult other)
+			{
+				return new SearchResult(null, GetMax(Max, other.Max), Count + other.Count);
+			}
+
+			private static int? GetMax(int? a, int? b)
+			{
+				if (!a.HasValue && !b.HasValue)
+					return null;
+				if (a.HasValue && b.HasValue)
+					return Math.Max(a.Value, b.Value);
+				return a.HasValue ? a.Value : b.Value;
+			}
+		}
+		public int Find(TreeNode<int> root, int k)
+		{
+			SearchResult searchResult = Search(root, new SearchResult(null, null, 0), k);
+			if (searchResult.Result.HasValue)
+				return searchResult.Result.Value;
+
+			throw new Exception(string.Format("{0}-th smallest item was not found", k));
+		}
+
+		private SearchResult Search(TreeNode<int> root, SearchResult acc, int k)
+		{
+			if (root == null)
+			{
+				return new SearchResult(null, 0, 0);
+			}
+			SearchResult fromLeft = Search(root.Left, new SearchResult(null, null, 0), k);
+			if (fromLeft.Result.HasValue)
+				return fromLeft;
+			if (fromLeft.Count == k - 1)
+				return new SearchResult(root.Data, 0, 0);
+
+			var rootNodeSearchResult = new SearchResult(null, root.Data, 1);
+			return Search(root.Right, acc.Union(fromLeft).Union(rootNodeSearchResult), k);
 		}
 	}
 }
--- AlgoSamples/Trees/TreeNode.cs
+++ AlgoSamples/Trees/TreeNode.cs
@@ -1,9 +1,17 @@
-﻿namespace Trees
+﻿using System.Diagnostics;
+
+namespace Trees
 {
+	[DebuggerDisplay("{Data}, {Left}, {Right}")]
 	public class TreeNode<TData>
 	{
 		public TData Data { get; set; }
 		public TreeNode<TData> Left { get; set; }
 		public TreeNode<TData> Right { get; set; }
+
+		public override string ToString()
+		{
+			return string.Format("{0}, {1}, {2}", Data, Left, Right);
+		}
 	}
 }
